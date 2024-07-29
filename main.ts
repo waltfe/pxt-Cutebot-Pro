e386e99c2eaf00786120691d8ec05a13fa07e592
鄢梦阳 <510857289@qq.com>
2024-07-29 17:38:52 +0800

    no msg

--- main.ts
+++ main.ts
@@ -1584,6 +1584,6 @@ namespace CutebotPro {
         i2cBuffer[5] = 0x00;
         i2cBuffer[6] = 0x88;
         pins.i2cWriteBuffer(i2cAddr, i2cBuffer)
-        return = pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(i2cAddr, NumberFormat.UInt8LE, false)
     }
 }
